7bb11267d39d711fdcf7fcdb56c516b10a3d4ba5
Ihor Khanchuk <mfc2005@ukr.net>
2021-08-15 18:57:33 +0300

    Init

---
+++ index.js
@@ -0,0 +1,18 @@
+class Observable {
+    observers = [];
+    constructor(){
+
+    }
+
+    subscribe(observerFunction) {
+        this.observers.push(observerFunction);
+    }
+
+    emmit(dataToEmmit) {
+        this.observers.forEach(observer => {
+            observer(dataToEmmit)
+        })
+    }
+}
+
+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
